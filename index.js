1f43a8e2ef2b960be391bd4b9ef9f07856721a6b
chris <chrishethertonweb@gmail.com>
2022-07-27 21:13:15 +0200

    ex 8 all set

--- index.js
+++ index.js
@@ -79,3 +79,16 @@ function mergeTwoArrays(arrayOne, arrayTwo) {
     } return (newArray)
 }
 console.log(mergeTwoArrays([1, 2, 3,], [2, 30, 1]));
+
+// EX 8
+
+function remove(array) {
+    // for (let i = 0; i <= array.length; i++) {
+    //     if (!array[i] || array[i] == false) {
+    //         array.splice([i], 1)
+    //     }
+    // }
+
+    return array
+}
+console.log(remove([NaN, 0, 15, false, -22, '', undefined, 47, null]));
